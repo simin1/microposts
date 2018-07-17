8a46ea6244c49fdfbe8ee483b39f9ff7f6958132
simin <s24.tyk@gmail.com>
2018-07-17 13:21:15 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,20 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @micropost = current_user.microposts.build  # form_for 用
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
+    end
+  end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 end
